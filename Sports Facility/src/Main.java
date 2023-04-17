4feb72b0c455037d1810aec772e147b7e38f38a6
axel <axel.do@live.com.ar>
2023-04-17 10:01:49 -0300

    nada

--- Sports Facility/src/Main.java
+++ Sports Facility/src/Main.java
@@ -1,7 +1,4 @@
-import Model.Building;
-import Model.MultiGymnasium;
-import Model.OfficeBuilding;
-import Model.SportsFacility;
+import Model.*;
 
 public class Main {
     public static void main(String[] args) {
--- Sports Facility/src/Model/test.java
+++ Sports Facility/src/Model/test.java
@@ -1,3 +1,5 @@
+package Model;
+
 import Model.Building;
 
 import java.util.ArrayList;
